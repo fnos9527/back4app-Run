8bc4d3018e14ebfd1fd8bbc4c32af971277fcece
fnos9527 <fnos9527@gmail.com>
2026-04-20 10:21:05 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ===== 只需要修改这里 =====
-GITHUB_TOKEN="你的GitHub_Token"
+GITHUB_TOKEN="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"
 # =========================
 
 GITHUB_API="https://api.github.com/repos/fnos9527/back4app-Run/actions/workflows/redeploy.yml/dispatches"
